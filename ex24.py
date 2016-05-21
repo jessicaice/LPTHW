c7bf3c19f51e27d3d90c3f98cdf5db1f224b2dc3
Jessica <jessica.ice.007@gmail.com>
2016-05-21 17:00:44 -0400

    Update ex24.py

--- ex24.py
+++ ex24.py
@@ -36,4 +36,4 @@ print "With a starting point of: %d crates." (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crates." %secret_formula(starting_point)
+print "We'd have %d beans, %d jars, and %d crates." %secret_formula(start_point)
