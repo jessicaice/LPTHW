dbc602a4a950d61730e20c13c291d8ea4b4be20e
Jessica <jessica.ice.007@gmail.com>
2016-05-21 17:01:22 -0400

    Update ex24.py

--- ex24.py
+++ ex24.py
@@ -25,7 +25,7 @@ print "This should be five: %s" % five
 def secret_formula(started):
   jelly_beans = started * 500
   jars = jelly_beans / 1000
-  creates = jars / 100
+  crates = jars / 100
   return jelly_beans, jars, crates
   
 start_point = 10000
