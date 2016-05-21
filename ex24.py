756de399fc0bfb3e0f5817cbd00c664758f8ab7a
Jessica <jessica.ice.007@gmail.com>
2016-05-21 17:02:52 -0400

    Update ex24.py

--- ex24.py
+++ ex24.py
@@ -31,9 +31,10 @@ def secret_formula(started):
 start_point = 10000
 beans, jars, crates = secret_formula(start_point) 
 
-print "With a starting point of: %d crates." (beans, jars, crates)
+print "With a starting point of: %d crates." % start_point
+print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crates." %secret_formula(start_point)
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
