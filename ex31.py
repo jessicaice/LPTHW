3b0e2f2a590570211c3b3c08213859288edfddfc
Jessica <jessica.ice.007@gmail.com>
2016-05-29 09:54:32 -0400

    Create Exercise 31

---
+++ ex31.py
@@ -0,0 +1,8 @@
+#Exercise 31: Making Decision
+#Learn Python the Hard Way
+#Created by Jessica Ice
+#Created on May 29th
+
+print "You enter a dark room with two doors. Do you go through door \
+1 or door 2?"
+
